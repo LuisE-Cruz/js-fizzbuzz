8eb4ff0bf6f27c5ace4e4c8848335cc5d743bcc2
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-02 15:23:43 +0100

    Inizo progetto FizzBuzz

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// FizzBuzz
+// stampa numeri da 1 a 100
+// multipli di 3 Fizz
+// multipli di 5 Buzz
+// multipli di entrambi FizzBuzz
+let fizz
+// let Buzz = " ";
+// let FizzBuzz = " ";
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0) {
+        let i = "FIZZ";
+        console.log(i);
+    }
+   console.log(i);
+}
+
