3d0f84a3f198c02392f14630914fc31c8eaa61d6
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-02 15:55:38 +0100

    fatto il ciclo che sostituisce già i numeri

--- js/script.js
+++ js/script.js
@@ -2,16 +2,27 @@
 // stampa numeri da 1 a 100
 // multipli di 3 Fizz
 // multipli di 5 Buzz
-// multipli di entrambi FizzBuzz
-let fizz
-// let Buzz = " ";
-// let FizzBuzz = " ";
+// multipli di entrambi FizzBuzzc
+const fizz = "Fizz"
+const buzz = "Buzz";
+const fizzBuzz = "FizzBuzz ";
 
-for (let i = 0; i <= 100; i++) {
-    if (i % 3 === 0) {
-        let i = "FIZZ";
-        console.log(i);
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 15 === 0) {
+
+        console.log(fizzBuzz);
+
+    } else if (i % 3 === 0) {
+
+        console.log(fizz);
+        
+    } else if (i % 5 === 0) {
+        
+        console.log(buzz);
+    } else {
+        console.log(i)
     }
-   console.log(i);
+
 }
 
